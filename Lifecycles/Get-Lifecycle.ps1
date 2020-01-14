c4adf22d040b3288f3bcba18e6e27cd8a7fc64d1
Michael Levan <mlevan1992@outlook.com>
2020-01-14 16:09:38 -0500

    List all lifecycles

---
+++ Lifecycles/Get-Lifecycle.ps1
@@ -0,0 +1,38 @@
+function Get-Projects {
+    [cmdletbinding(SupportsShouldProcess, ConfirmImpact = 'low')]
+    param(
+        [parameter(Position = 0, Mandatory = $true)]
+        [string]$OctopusBaseURL
+    )
+
+    begin { }
+    process {
+        try {
+            $octopusAPIKey = Read-Host 'Input API Key' -AsSecureString
+            $headers = @{ "X-Octopus-ApiKey" = $octopusAPIKey }
+
+            $header = @{ "X-Octopus-ApiKey" = $octopusAPIKey | ConvertFrom-SecureString -AsPlainText }
+
+            $listLifecycles = $(Invoke-WebRequest -Method GET -Uri $OctopusBaseURL/lifecycles -Headers $header).content
+            $convert = $listLifecycles | ConvertFrom-Json
+        }
+
+        catch {
+            Write-Warning 'An error has occurred. Please confirm your API key and Octopus Base URL is correct'
+            $PSCmdlet.ThrowTerminatingError($_)
+        }
+
+        try {
+            $obj = [pscustomobject] @{
+                'ProjectName'           = $convert.Items.Name
+                'ID'                    = $convert.Items.id
+            }
+
+            $obj | fl
+        }
+        catch {
+            $PSCmdlet.ThrowTerminatingError($_)
+        }
+    }
+    end { }
+}
